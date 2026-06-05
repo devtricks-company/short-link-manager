d64fc770d65d9f87f5b0626d9681154b68feb252
dev.team <dev.team@gradegy.com>
2026-06-05 18:20:41 +0330

    add zod schema for sing-in form

--- src/lib/actions/sign-in.ts
+++ src/lib/actions/sign-in.ts
@@ -2,16 +2,23 @@
 
 import {auth} from '@/lib/auth/server';
 import { redirect } from 'next/navigation';
+import { signInSchema } from '../validations/sign-in';
 
 export async function signInWithEmail(
     _prevState:{error:string} | null,
     formData: FormData
 ){
+    const parsed = signInSchema.safeParse({
+        email: formData.get('email'),
+        password: formData.get('password'),
+    });
 
-    const {error} = await auth.signIn.email({
-        email:formData.get("email") as string,
-        password:formData.get("password") as string
-    })
+    if (!parsed.success)
+        return { error: parsed.error.issues[0].message };
+
+    const { email, password } = parsed.data;
+
+    const {error} = await auth.signIn.email({ email, password })
 
     if(error)
             return  { error: error.message || 'Failed to sign in. Try again' };
