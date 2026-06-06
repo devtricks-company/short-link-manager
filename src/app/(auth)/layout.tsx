c39a17c7db44a5fba8553d5e422e480c806ffe2a
dev.team <dev.team@gradegy.com>
2026-06-06 21:14:19 +0330

    change the protected route with group routing

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@/lib/auth/server';
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth.getSession();
+  console.log('session',session);
+  if (session.data) {
+    redirect('/dashboard');
+  }
+
+  return <>{children}</>;
+}
--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import { DashboardView } from '@/views/dashboard/dashboard.view';
 import { LinksTableServer } from '@/views/dashboard/links-table-server';
 import { LinksTableSkeleton } from '@/views/dashboard/links-table-skeleton';
+
+export const metadata: Metadata = {
+  title: 'LinkSnap - Dashboard',
+};
 
 export default function DashboardPage() {
   return (
